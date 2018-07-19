7c36e467b2c30e461695f397d4e566786629710e
Alex Smith <alexsmith716@outlook.com>
2018-07-18 22:20:47 -0400

    modification(Webpack, Server): WEBPACK moved ProvidePlugin to only Config for dev and prod. SERVER removed Bootstrap navbarCollapse sendFile middlewares.

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -82,5 +82,32 @@ module.exports = {
   //   hints: false
   // },
 
-  plugins: [],
+  plugins: [
+
+    // ProvidePlugin: Whenever the identifier is encountered as free variable in a module, 
+    //    the module is loaded automatically and the identifier is filled with the exports of 
+    //    the loaded module (of property in order to support named exports).
+
+    // To automatically load jquery point variables it exposes to the corresponding node module
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      jquery: 'jquery',
+      Popper: ['popper.js', 'default'],
+      Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+      Button: "exports-loader?Button!bootstrap/js/dist/button",
+      Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+      Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+      Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+      Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+      Util: "exports-loader?Util!bootstrap/js/dist/util",
+    }),
+
+  ],
 };
+
+
