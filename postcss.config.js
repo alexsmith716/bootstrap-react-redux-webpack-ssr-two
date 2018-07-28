fe54479b5feffb112b98fdde2f3ab0c741b46f3b
Alex Smith <alexsmith716@outlook.com>
2018-07-28 16:52:04 -0400

    refactor(postcss-config): modified postcss config.

--- postcss.config.js
+++ postcss.config.js
@@ -1,46 +1,13 @@
-// var autoprefixer = require('autoprefixer')
-// var cssCustomProperties = require('postcss-custom-properties')
-// var postcssCalc = require('postcss-calc')
-// 
-// module.exports = {
-//   plugins: [
-//     autoprefixer(),
-//     cssCustomProperties(),
-//     postcssCalc()
-//   ]
-// }
 
-// http://cssnext.io/features/
+// postcss-loader exposes Context (ctx) to the config file, making postcss.config.js dynamic
 
-module.exports = {
-  use: [
-    'postcss-import',
-    'postcss-url',
-    'postcss-preset-env',
-    'postcss-browser-reporter',
-    'postcss-reporter',
-  ]
-};
-
-
-// https://github.com/postcss/postcss-loader#context-ctx
-
-
-// module.exports = ({ file }) => ({
-//   plugins: {
-//     'postcss-import': { root: file.dirname },
-//     'postcss-url': [
-//       { filter: './**.*', url: asset => `./${asset.url}` }, // [relative path]
-//     ],
-//     // http://cssnext.io/usage, http://browserl.ist
-//     'postcss-cssnext': {
-//       browsers: ['last 2 version'],
-//     },
-//     // add your "plugins" here
-//     // ...
-//     // and if you want to compress,
-//     // just use css-loader option that already use cssnano under the hood
-//     'postcss-browser-reporter': {},
-//     'postcss-reporter': {},
-//   }
-// });
+module.exports = ({ file }) => ({
+  plugins: {
+    'postcss-import': { root: file.dirname },
+    'postcss-cssnext': {
+      browsers: ['last 2 version'],
+    },
+    'postcss-browser-reporter': {},
+    'postcss-reporter': {},
+  }
+})
