e93ff53e08d0bc06edb7daf93d0bd6c4d2aef102
Alex Smith <alexsmith716@outlook.com>
2018-08-05 16:10:01 -0400

    refactor(Removed Babel Stage Presets): Removed Babel Stage Presets and made necessary changes.

--- client/redux/createStore.js
+++ client/redux/createStore.js
@@ -1,9 +1,8 @@
 import { createStore as _createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import { routerMiddleware } from 'react-router-redux';
+import { connectRouter, routerMiddleware } from 'connected-react-router';
 import { createPersistoid, persistCombineReducers, REGISTER } from 'redux-persist';
 import clientMiddleware from './middleware/clientMiddleware';
 import createReducers from './reducer';
-
 
 function combine(reducers, persistConfig) {
   if (persistConfig) {
@@ -18,9 +17,11 @@ export function inject(store, reducers, persistConfig) {
     store.asyncReducers[name] = reducer.__esModule ? reducer.default : reducer;
   });
 
+  // get a new root reducer
   store.replaceReducer(combine(createReducers(store.asyncReducers), persistConfig));
 }
-
+ 
+// NoOp action does nothing returns state and 'none' effect
 function getNoopReducers(reducers, data) {
   if (!data) return {};
   return Object.keys(data).reduce(
@@ -29,43 +30,51 @@ function getNoopReducers(reducers, data) {
   );
 }
 
-
 // =======================================================================================
 // =======================================================================================
 
+export default function createStore({ history, data, helpers, persistConfig }) {
 
-export default function createStore({ history, data, providers, persistConfig }) {
+  // https://redux.js.org/advanced/middleware
+  const middleware = [clientMiddleware(helpers), routerMiddleware(history)];
 
-  const middleware = [clientMiddleware(providers), routerMiddleware(history)];
-
-  if (__CLIENT__ && __DEVELOPMENT__) {
-    const logger = require('redux-logger').createLogger({
-      collapsed: true
-    });
-    middleware.push(logger.__esModule ? logger.default : logger);
-  }
+  // Redux logger
+  // if (__CLIENT__ && __DEVELOPMENT__) {
+  //   const logger = require('redux-logger').createLogger({
+  //     collapsed: true
+  //   });
+  //   middleware.push(logger.__esModule ? logger.default : logger);
+  // }
 
   const enhancers = [applyMiddleware(...middleware)];
 
-  if (__CLIENT__ && __DEVTOOLS__) {
-    const { persistState } = require('redux-devtools');
-    const DevTools = require('../containers/DevTools/DevTools');
+  // Redux DevTool
+  // if (__CLIENT__ && __DEVTOOLS__) {
+  //   const { persistState } = require('redux-devtools');
+  //   const DevTools = require('../containers/DevTools/DevTools');
 
-    Array.prototype.push.apply(enhancers, [
-      window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument(),
-      persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
-    ]);
-  }
+  //   Array.prototype.push.apply(enhancers, [
+  //     window.devToolsExtension ? window.devToolsExtension() : DevTools.instrument(),
+  //     persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+  //   ]);
+  // }
 
+  // Composed Enhancers
   const finalCreateStore = compose(...enhancers)(_createStore);
+
   const reducers = createReducers();
   const noopReducers = getNoopReducers(reducers, data);
+
+  // =======================================================================================
+
   const store = finalCreateStore(combine({ ...noopReducers, ...reducers }, persistConfig), data);
 
   store.asyncReducers = {};
+
   store.inject = _reducers => inject(store, _reducers, persistConfig);
 
-  // persistConfig > from client
+  // =======================================================================================
+
   if (persistConfig) {
     const persistoid = createPersistoid(persistConfig);
     store.subscribe(() => {
@@ -73,6 +82,8 @@ export default function createStore({ history, data, providers, persistConfig })
     });
     store.dispatch({ type: REGISTER });
   }
+
+  // =======================================================================================
 
   // https://github.com/59naga/babel-plugin-add-module-exports
   if (__DEVELOPMENT__ && module.hot) {
--- shared/routes.js
+++ shared/routes.js
@@ -3,51 +3,51 @@
 // https://mjrussell.github.io/redux-auth-wrapper/docs/Getting-Started/ReactRouter4.html
 // https://github.com/mjrussell/redux-auth-wrapper/tree/master/examples/react-router-4
 import { connectedReduxRedirect } from 'redux-auth-wrapper/history4/redirect';
-import { routerActions } from 'react-router-redux';
+import { routerActions } from 'connected-react-router';
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 import { App, Home, NotFound } from '../client/containers';
 
-import Register from '../client/containers/Register/Loadable';
-import LoginSuccess from '../client/containers/LoginSuccess/Loadable';
-
-import About from '../client/containers/About/Loadable';
-import AboutOne from '../client/containers/AboutOne/Loadable';
-import AboutToo from '../client/containers/AboutToo/Loadable';
-import AboutThree from '../client/containers/AboutThree/Loadable';
-import AboutFour from '../client/containers/AboutFour/Loadable';
-import Login from '../client/containers/Login/Loadable';
-
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-// Helper 'connectedRouterRedirect' builds HOC 'isAuthenticated/isNotAuthenticated'
-// config {} passed in to 'connectedRouterRedirect' determines access to route
-// apply 'isAuthenticated/isNotAuthenticated' to Component(s) ('LoginSuccess' && 'Register')
-// pass Component ('LoginSuccess' / 'Register') to HOC ('isAuthenticated' / 'isNotAuthenticated')
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-const isAuthenticated = connectedReduxRedirect({
-  // url to redirect if check fails
-  redirectPath: '/login',
-  // If authenticatedSelector is true, wrapper will not redirect
-  // So if there is no user data, then we show the page
-  authenticatedSelector: state => state.auth.user !== null,
-  // dispatch a redux action to navigate - pass the redux action creator to redirectAction
-  redirectAction: routerActions.replace,          
-  wrapperDisplayName: 'UserIsAuthenticated'
-});
-
-const isNotAuthenticated = connectedReduxRedirect({
-  // url to redirect if check fails
-  redirectPath: '/',
-  // If authenticatedSelector is true, wrapper will not redirect
-  authenticatedSelector: state => state.auth.user === null,
-  // dispatch a redux action to navigate - pass the redux action creator to redirectAction
-  redirectAction: routerActions.replace,
-  wrapperDisplayName: 'UserIsAuthenticated',
-  // prevent adding the query parameter when we send the user away from the Register page
-  allowRedirectBack: false
-});
+// import Register from '../client/containers/Register/Loadable';
+// import LoginSuccess from '../client/containers/LoginSuccess/Loadable';
+// 
+// import About from '../client/containers/About/Loadable';
+// import AboutOne from '../client/containers/AboutOne/Loadable';
+// import AboutToo from '../client/containers/AboutToo/Loadable';
+// import AboutThree from '../client/containers/AboutThree/Loadable';
+// import AboutFour from '../client/containers/AboutFour/Loadable';
+// import Login from '../client/containers/Login/Loadable';
+// 
+// // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// // Helper 'connectedRouterRedirect' builds HOC 'isAuthenticated/isNotAuthenticated'
+// // config {} passed in to 'connectedRouterRedirect' determines access to route
+// // apply 'isAuthenticated/isNotAuthenticated' to Component(s) ('LoginSuccess' && 'Register')
+// // pass Component ('LoginSuccess' / 'Register') to HOC ('isAuthenticated' / 'isNotAuthenticated')
+// // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// 
+// const isAuthenticated = connectedReduxRedirect({
+//   // url to redirect if check fails
+//   redirectPath: '/login',
+//   // If authenticatedSelector is true, wrapper will not redirect
+//   // So if there is no user data, then we show the page
+//   authenticatedSelector: state => state.auth.user !== null,
+//   // dispatch a redux action to navigate - pass the redux action creator to redirectAction
+//   redirectAction: routerActions.replace,          
+//   wrapperDisplayName: 'UserIsAuthenticated'
+// });
+// 
+// const isNotAuthenticated = connectedReduxRedirect({
+//   // url to redirect if check fails
+//   redirectPath: '/',
+//   // If authenticatedSelector is true, wrapper will not redirect
+//   authenticatedSelector: state => state.auth.user === null,
+//   // dispatch a redux action to navigate - pass the redux action creator to redirectAction
+//   redirectAction: routerActions.replace,
+//   wrapperDisplayName: 'UserIsAuthenticated',
+//   // prevent adding the query parameter when we send the user away from the Register page
+//   allowRedirectBack: false
+// });
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -55,14 +55,14 @@ const routes = [{
   component: App,
   routes: [
     { path: '/', exact: true, component: Home },
-    { path: '/about', component: About },
-    { path: '/aboutone', component: AboutOne },
-    { path: '/abouttoo', component: AboutToo },
-    { path: '/aboutthree', component: AboutThree },
-    { path: '/aboutfour', component: AboutFour },
-    { path: '/login', component: Login },
-    { path: '/login-success', component: isAuthenticated(LoginSuccess) },
-    { path: '/register', component: isNotAuthenticated(Register) },
+    // { path: '/about', component: About },
+    // { path: '/aboutone', component: AboutOne },
+    // { path: '/abouttoo', component: AboutToo },
+    // { path: '/aboutthree', component: AboutThree },
+    // { path: '/aboutfour', component: AboutFour },
+    // { path: '/login', component: Login },
+    // { path: '/login-success', component: isAuthenticated(LoginSuccess) },
+    // { path: '/register', component: isNotAuthenticated(Register) },
     { component: NotFound },
   ],
 }];
